41e69575da4e8ed3ebc435af30f1669f6ad290e4
unknown <hanhcu7777@gmail.com>
2024-03-23 18:36:25 +0700

    added error handling, error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="min-h-dvh bg-system-black p-5">
+	<h1 class="text-2xl text-white">{$page.error?.message}</h1>
+</div>
--- src/routes/[username]/+layout.ts
+++ src/routes/[username]/+layout.ts
@@ -1,5 +1,6 @@
 import type { ProfileResponse, UserGalleriesReponse } from '$lib/types.js';
 import { api } from '$lib/utils.js';
+import { error } from '@sveltejs/kit';
 
 export async function load({ fetch, params }) {
 	const galleriesP = api.get(
@@ -8,7 +9,9 @@ export async function load({ fetch, params }) {
 	) as Promise<UserGalleriesReponse>;
 	const profileP = api.get(fetch, `users/profile/${params.username}`) as Promise<ProfileResponse>;
 	const [galleries, profile] = await Promise.all([galleriesP, profileP]);
-
+	if (!galleries.result || !profile.result) {
+		error(404, 'User not found!!');
+	}
 	return {
 		gallery: galleries.result.data.gals,
 		profile: profile.result.data
